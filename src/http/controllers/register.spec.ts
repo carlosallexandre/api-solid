fdeb5d718e4f0f8361a21f22ae4c878d696bf8be
carlosallexandre <carlos_costarodrigues@outlook.com>
2024-01-06 19:31:18 -0300

    test: add register e2e

--- src/http/controllers/register.spec.ts
+++ src/http/controllers/register.spec.ts
@@ -1,7 +1,25 @@
 // @vitest-environment prisma
+import request from 'supertest'
+import { afterAll, beforeAll, expect, it, test } from 'vitest'
 
-import { test } from 'vitest'
+import { app } from '@/app'
 
-test('RegisterController', () => {
-  //
+test('Register (e2e)', () => {
+  beforeAll(async () => {
+    await app.ready()
+  })
+
+  afterAll(async () => {
+    await app.close()
+  })
+
+  it('should be able to register', async () => {
+    const response = await request(app.server).post('/users').send({
+      name: 'John Doe',
+      email: 'johndoe@example.com',
+      password: 'password123',
+    })
+
+    expect(response.statusCode).toBe(201)
+  })
 })
